3d69d68a0645a83f6773da2bb9de10f809d8549c
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2024-10-01 22:35:15 +0300

    Update csvmerger.py

--- src/csvmerger.py
+++ src/csvmerger.py
@@ -1,7 +1,7 @@
 import os
 bases = ['volume01']
 cwd = os.getcwd()
-paths = [path.join(cwd, p) for p in bases]
+paths = [os.path.join(cwd, p) for p in bases]
 #list_subfolders_with_paths = [f.path for f in os.scandir(path) if f.is_dir()]
 #print(list_subfolders_with_paths)
 #print(f"cwd {cwd}")
